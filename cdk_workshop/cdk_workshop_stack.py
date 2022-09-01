4aea5821f99683e3faf05f60b396432bf85ea62d
captivus <366332+captivus@users.noreply.github.com>
2022-09-01 12:17:29 -0500

    adding endpoint CloudFormation stack outputs

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -2,6 +2,7 @@ import imp
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
@@ -13,6 +14,14 @@ class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        @property
+        def hc_endpoint(self):
+            return self._hc_endpoint
+
+        @property
+        def hc_viewer_url(self):
+            return self._hc_viewer_url
 
         # Defines an AWS Lambda resource
         my_lambda = _lambda.Function(
@@ -29,16 +38,28 @@ class CdkWorkshopStack(Stack):
             downstream=my_lambda,
         )
 
-        apigw.LambdaRestApi(
+        gateway = apigw.LambdaRestApi(
             scope=self,
             id="Endpoint",
             handler=hello_with_hitcounter.handler,
         )
 
-        TableViewer(
+        tv = TableViewer(
             self,
             id='ViewHitCounter',
             title='Hello Hits!',
             table=hello_with_hitcounter.table,
             sort_by='-hits'
+        )
+
+        self._hc_endpoint = CfnOutput(
+            scope=self,
+            id='GatewayUrl',
+            value=gateway.url
+        )
+
+        self._hc_viewer_url = CfnOutput(
+            scope=self,
+            id='TableViewerUrl',
+            value=tv.endpoint
         )
